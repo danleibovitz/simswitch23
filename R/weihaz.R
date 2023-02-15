7c549de14c2f15a42a752316eca3f05c0740e0aa
Daniel Leibovitz <danleibovitz@gmail.com>
2023-02-15 17:15:50 +0100

    add R/ and man/ folders

---
+++ R/weihaz.R
@@ -0,0 +1,32 @@
+#' A hazard generator for a weibull distribution
+#'
+#' @param x A vector of positive real numbers at which to evaluate the hazard
+#' @param shape The shape parameter
+#' @param scale The scale parameter
+#'
+#' @return A vector of hazards evaluated at each element of argument 'x'
+#' @export
+#'
+#' @examples
+weihaz <- function(
+  x,
+  shape,
+  scale) {
+
+  # Defend against incorrect argument types
+  if (!is.numeric(shape) & !is.integer(shape)) stop("shape parameter must be numeric")
+  if (!is.numeric(scale) & !is.integer(scale)) stop("scale parameter must be numeric")
+  if (!is.numeric(x) & !is.integer(x)) stop("x parameter must be numeric")
+
+  # Defend against incorrect argument dimensions
+  if (length(shape) != 1) stop("'shape' must be of length 1")
+  if (length(scale) != 1) stop("'scale' must be of length 1")
+  if (length(x) < 1) stop("'x' must be a vector of at least length 1")
+
+  # Defend against incorrect argument magnitudes
+  if (shape <= 0) stop("shape parameter must be positive")
+  if (scale <= 0) stop("scale parameter must be positive")
+  if (range(x)[1] <= 0) stop("x parameter must all be positive")
+
+  return((shape/scale)*(x/scale)^(shape-1))
+}
