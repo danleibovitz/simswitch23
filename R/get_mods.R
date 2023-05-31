61589e7713c61656e3d9fcf52c736e640c78bff7
Daniel Leibovitz <danleibovitz@gmail.com>
2023-05-31 18:31:11 +0200

    add file for all get_model() functions

---
+++ R/get_mods.R
@@ -0,0 +1,26 @@
+
+
+get_unbiased <- function(){
+
+}
+
+get_itt <- function(){
+
+}
+
+get_pp <- function(){
+
+}
+
+get_ipcw <- function(){
+
+}
+
+get_rpsftm <- function(){
+
+}
+
+get_tse <- function(){
+
+}
+
