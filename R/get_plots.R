5330ff383cdb26e9f69b788c7061eb418151c0eb
Daniel Leibovitz <danleibovitz@gmail.com>
2023-05-31 18:32:00 +0200

    add file for all get_plot() functions

---
+++ R/get_plots.R
@@ -0,0 +1,33 @@
+
+
+get_unbiased_plot <- function(){
+
+}
+
+get_itt_plot <- function(){
+
+}
+
+get_pp_plot <- function(){
+
+}
+
+get_ipcw_plot <- function(){
+
+}
+
+get_rpsftm_plot <- function(){
+
+}
+
+get_tse_plot <- function(){
+
+}
+
+get_compar_plot <- function(){
+
+}
+
+get_bias_plot <- function(){
+
+}
