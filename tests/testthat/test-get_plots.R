5473addf515ccab64cb291c47fb6ffbe4ac63b3e
Daniel Leibovitz <danleibovitz@gmail.com>
2023-06-09 17:58:59 +0200

    add test file for get_plots

---
+++ tests/testthat/test-get_plots.R
@@ -0,0 +1,33 @@
+test_that("get_unbiased_plot works", {
+
+})
+
+test_that("get_itt_plot works", {
+
+})
+
+test_that("get_pp_plot works", {
+
+})
+
+test_that("get_ipcw_plot works", {
+
+})
+
+test_that("get_rpsftm_pot works", {
+
+})
+
+test_that("get_tse_plot works", {
+
+})
+
+test_that("get_compar_plot works", {
+
+})
+
+test_that("get_bias_plot works", {
+
+})
+
+
