265f2b761296c3be409b0290fa2d1155a375c0f0
Daniel Leibovitz <danleibovitz@gmail.com>
2023-06-09 17:56:10 +0200

    add helper functions skeletons

---
+++ R/helpers.R
@@ -0,0 +1,31 @@
+get_empty_estimate_frame <- function(){
+  data.frame(
+    unbiased = NA,
+    itt = NA,
+    pp = NA,
+    ipcw_est = NA,
+    rpsftm_est = NA,
+    tse = NA,
+    secondary_baseline_observed = NA,
+    switch_observed = NA,
+    switch_iter = NA, # how many times have we searched on the inner loop?
+    Minb0 = NA,
+    Minb1 = NA
+    )
+}
+
+get_default_params <- function(){
+
+}
+
+get_surv_params <- function(){
+
+}
+
+checkparams <- function(){
+
+}
+
+checksurvparams <- function(){
+
+}
